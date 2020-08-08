a513667e618885bb0e1930269f3c071620d4e13f
Gustavo <guga.rocha.silveira@hotmail.com>
2020-08-08 17:28:06 -0300

    Finished v.1.0 of aplication

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import giveClassesBgImg from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+
+function GiveClasses() {
+  const { goBack } = useNavigation()
+
+  function handleNavigateBack() {
+    goBack()
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground style={styles.content} resizeMode="contain" source={giveClassesBgImg}>
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar na nossa plataforma web
+        </Text>
+      </ImageBackground>
+
+      <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+};
+
+export default GiveClasses;
---
+++ server/README.md
@@ -0,0 +1,10 @@
+# Funcionalidades
+
+## conexão
+- Rota para listar o total de conexões realizadas;
+- Rota para criar uma nova conexão;
+
+## Aulas
+- Rota para criar uma aula;
+- Rota para listar aulas;
+  - Filtrar por matérias, dias da semana e horários
